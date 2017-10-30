343f3fd06727e172f378d1a5c54170a3e27ae832
Henning Håkonsen <henninghaakonsen@me.com>
2017-10-30 13:06:52 +0100

    Minor changes to app

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url : 'mongodb://sensoruser:sensoruser@ds151963.mlab.com:51963/sensordb'
+  //url : 'mongodb://sensoruser:sensoruser@ds151963.mlab.com:51963/sensordb'
+  url: 'mongodb://localhost:27017/db'
 }

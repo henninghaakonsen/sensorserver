d839781a1f758cab09f2250871a30c2b5814d847
Henning Håkonsen <henninghaakonsen@me.com>
2019-08-10 14:44:16 +0200

    Fjerner https.

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ var path = require("path");
 var public = __dirname + "/app/public/";
 const port = process.env.PORT || 9000;
 const router = require("./app/routes/router");
-const fs = require("fs");
-const https = require("https");
 
 server.use(express.static(path.join(__dirname, "app/public")));
 server.enable("trust proxy");
@@ -19,17 +17,9 @@ server.use(bodyParser.json({ limit: "200mb", extended: true }));
 server.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 router.setupRouter(server);
 
-https
-    .createServer(
-        {
-            key: fs.readFileSync("server.key"),
-            cert: fs.readFileSync("server.cert")
-        },
-        server
-    )
-    .listen(port, () => {
-        console.log(
-            "info",
-            `Worker ${process.pid} : started http server on ` + port
-        );
-    });
+server.listen(port, () => {
+    console.log(
+        "info",
+        `Worker ${process.pid} : started https server on ` + port
+    );
+});
